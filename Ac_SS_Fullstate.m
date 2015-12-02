8caec7124ee5219326cf964c17b9927368184352
Abhinav Kunapareddy <akunapa1@jhu.edu>
2015-12-02 04:29:51 -0500

    Created shaded bar on y-axis. Todo- Proper shading, makethis into a function

--- Ac_SS_Fullstate.m
+++ Ac_SS_Fullstate.m
@@ -113,6 +113,73 @@ ylabel('Output Signal after filtering') % y-axis label
 
 
 
+%{
+
+%%
+
+hist_axes = gca;
+
+h_fig = ancestor(hist_axes,'figure');
+
+% Get x/y limits of axes using axis
+limits = axis(hist_axes);
 
 
+
+
+% Cache the original axes position so that axes can be repositioned to
+% occupy the space used by the colorstripe if nextplot clears the histogram
+% axes.
+original_axes_pos = get(hist_axes,'Position');
+
+% In GUIDE, default axes units are characters. In order for axes repositiong
+% to behave properly, units need to be normalized.
+hist_axes_units_old = get(hist_axes,'units');
+set(hist_axes,'Units','Normalized');
+% Get axis position and make room for color stripe.
+pos = get(hist_axes,'pos');
+stripe = 0.075;
+set(hist_axes,'pos',[pos(1)+stripe*pos(3) pos(2) (1-stripe)*pos(3) pos(4)])
+set(hist_axes,'Units',hist_axes_units_old);
+
+set(hist_axes,'xticklabel','')
+
+% Create axis for stripe
+stripe_axes = axes('Parent',get(hist_axes,'Parent'),...
+                'Position', [pos(1) pos(2) stripe*pos(3) pos(4)]);
+				 				 
+limits = axis(stripe_axes);
+line(limits([1 2 2 1 1]),limits([3 3 4 4 3]),...
+       'LineStyle','-',...
+       'Parent',stripe_axes,...
+       'Color',get(stripe_axes,'XColor'));
+
+   % Create color stripe
+    n=length(x(:,1));
+    binInterval = 1/n;
+    xdata = [binInterval/2 1-(binInterval/2)];
+    %limits(1:2) = range;
+
+        C = (1:n)/n;
+
+        x_range=x(:,1);
+    y_range=1.5*x_range.^2+5*x_range;
+        C1 = mat2gray(y_range);
+        
+    % image(X,Y,C) where C is the RGB color you specify. 
+    image(xdata,[0 1],repmat(C1, [1 1 3]),'Parent',stripe_axes);
+
+
+%set(stripe_axes,'yticklabel','')
+%axis(stripe_axes,limits);
+
+% Put a border around the stripe.
+
+%figure;
+%plot(t,C,t,C1)
+figure;
+plot(t,x_range);figure;plot(t,y_range)
+
+
+%}
 
